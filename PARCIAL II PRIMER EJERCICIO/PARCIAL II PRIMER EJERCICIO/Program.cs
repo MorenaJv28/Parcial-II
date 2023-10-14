72a875a71de8296301537e2ebb63eb148bb7e278
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-10-14 15:53:06 -0600

    EJERCICIO1

---
+++ PARCIAL II PRIMER EJERCICIO/PARCIAL II PRIMER EJERCICIO/Program.cs
@@ -0,0 +1,25 @@
+﻿//EJERCICIO 1
+Console.WriteLine("Bienvenido al Sistema");
+Console.WriteLine("Favor ingrese la clave principal:");
+
+string ClavePrincipal = "AdministradoR";
+string Clave = Console.ReadLine();
+
+if (ValidarConstraseña(Clave, ClavePrincipal))
+{
+    Console.WriteLine("Acceso aceptado.  Puede realizar las configuraciones de respaldo.");
+}
+else
+{
+    Console.WriteLine("Acceso rechazado. La clave ingresada no es válida.");
+}
+
+static string N(string input)
+{
+    return input.Trim().ToLower();
+}
+
+static bool ValidarConstraseña(string input, string Password)
+{
+    return N(input) == N(Password);
+}
