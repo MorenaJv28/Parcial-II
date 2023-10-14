2b10752c82211dddf8442541eb735de513d8ea1d
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-10-14 16:05:18 -0600

    EJERCICIO 3

---
+++ PARCIAL II EJERCICIO 3/PARCIAL II EJERCICIO 3/Program.cs
@@ -0,0 +1,23 @@
+﻿//EJERCICIO 3
+
+
+DateTime FF = new DateTime(2023, 11, 18, 15, 0, 0);
+
+DateTime Fecha = DateTime.Now;
+
+TimeSpan DR = FF - Fecha;
+int DF = (int)DR.TotalDays;
+
+string F1 = Fecha.ToString("dd/MM/yyyy");
+string F2 = Fecha.ToString("dd MMM yyyy");
+string F3 = Fecha.ToString("yyyy MMM dd");
+Console.WriteLine();
+Console.WriteLine("Finalizacizaciòn de Actividades Academicas");
+Console.WriteLine();
+Console.WriteLine("Fecha y hora del dia de hoy : " + Fecha);
+Console.WriteLine();
+Console.WriteLine("Timpo restante para el parcial: " + DF + " días");
+Console.WriteLine();
+Console.WriteLine("Forma 1: " + F1);
+Console.WriteLine("Forma 2: " + F2);
+Console.WriteLine("Forma 3: " + F3);
